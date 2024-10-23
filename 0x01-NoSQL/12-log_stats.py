24ee0134b3c039aa185918210b9190abc5d07ff9
LibrazionPc <znwajei@gmail.com>
2024-10-23 02:40:07 +0100

    Db

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+""" Log stats in Python"""
+
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    """ provides some stats about Nginx logs stored in MongoDB"""
+    con = MongoClient('mongodb://localhost:27017')
+    collection = con.logs.nginx
+
+    print(f'{collection.estimated_document_count()} logs')
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('Methods:')
+
+    for req in methods:
+        print('\tmethods {}: {}'.format(req,
+              collection.count_documents({'method': req})))
+
+    print('{} status check'.format(collection.count_documents(
+          {'method': 'GET', 'path': '/status'})))
